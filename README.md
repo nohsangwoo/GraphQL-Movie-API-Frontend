aff1cea099a3c75aa2593173e7553bec433bba6f
nohsangwoo <nsgr12@naver.com>
2020-10-18 23:27:17 +0900

    router and reset.css

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # styled-componenets, react-router-dom, react-apollo
+
+https://www.apollographql.com/docs/react/get-started/
+여기서 하라는대로 함
+
+# settig and reset CSS적용
+
+https://cssreset.com/scripts/eric-meyer-reset-css/
--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,15 @@
 import React from "react";
+import { HashRouter as Router, Route } from "react-router-dom";
+import Home from "../routes/Home";
+import Detail from "../routes/Detail";
 
 function App() {
-  return <div className="App">hi</div>;
+  return (
+    <Router>
+      <Route exact path="/" component={Home}></Route>
+      <Route path="/:id" component={Detail}></Route>
+    </Router>
+  );
 }
 
 export default App;
