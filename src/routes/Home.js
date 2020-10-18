b7dbb46b1bc07070ab690ec3f8a723f230b81601
nohsangwoo <nsgr12@naver.com>
2020-10-19 00:01:34 +0900

    GET MOVIES query

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,3 +1,23 @@
 import React from "react";
+import { useQuery, gql } from "@apollo/client";
 
-export default () => <h1>Home</h1>;
+const GET_MOVIES = gql`
+  {
+    movies {
+      id
+      medium_cover_image
+    }
+  }
+`;
+
+export default () => {
+  // GET_MOVIES의 쿼리를 가져와서 사용하는데 loading여부 , error여부 , data의 확인을 props 받아올수있음
+  const { loading, error, data } = useQuery(GET_MOVIES);
+  console.log(loading, error);
+  if (loading) {
+    return <h1>Loading...</h1>;
+  } else {
+    const { movies } = data;
+    return movies.map((m, i) => <h1 key={i}>{m.id}</h1>);
+  }
+};
